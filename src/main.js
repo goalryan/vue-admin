617983237736563c28a19caceed3d9682b780a02
goalryan <goalryan@qq.com>
2017-10-13 17:56:02 +0800

    添加数据访问类

--- src/main.js
+++ src/main.js
@@ -6,11 +6,12 @@ import EcUI from './components';
 import VueRouter from 'vue-router'
 import store from './vuex/store'
 import Vuex from 'vuex'
+import Axios from 'axios'
+import VueAxios from './utils/vueAxios'
 import routes from './routes'
 import Mock from './mock'
 Mock.bootstrap();
 import 'element-ui/lib/theme-default/index.css'
-//import './assets/theme/theme-green/index.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './assets/index.less';
 
@@ -18,29 +19,27 @@ Vue.use(ElementUI)
 Vue.use(EcUI)
 Vue.use(VueRouter)
 Vue.use(Vuex)
+Vue.use(VueAxios, Axios);
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 router.beforeEach((to, from, next) => {
-  if (to.path == '/login') {
-    sessionStorage.removeItem('user');
-  }
-  let user = JSON.parse(sessionStorage.getItem('user'));
-  if (!user && to.path != '/login') {
-    next({ path: '/login' })
-  } else {
-    next()
-  }
+    if (to.path == '/login') {
+        sessionStorage.removeItem('user');
+    }
+    let user = JSON.parse(sessionStorage.getItem('user'));
+    if (!user && to.path != '/login') {
+        next({ path: '/login' })
+    } else {
+        next()
+    }
 })
 
 new Vue({
-  //el: '#app',
-  //template: '<App/>',
-  router,
-  store,
-  //components: { App }
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
 
